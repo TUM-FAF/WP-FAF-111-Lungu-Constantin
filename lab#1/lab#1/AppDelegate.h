67cd995f70e65511dc1e56d3b48ffcd6319a993a
Constantin Lungu <koctake@gmail.com>
2013-02-27 19:39:11 +0200

    Application finished
    I finished the application, I will attach the README.md file with it so
    that every thing going on in the app is explained in the readme file.
    The app is built on Xcode 4.3.3 running on Mac OS X 10.7.3

--- lab#1/lab#1/AppDelegate.h
+++ lab#1/lab#1/AppDelegate.h
@@ -4,10 +4,23 @@
 
 @property (assign) IBOutlet NSWindow *window;
 
+// declare the outlets
+
 @property (weak) IBOutlet NSTextField *fruitTabOne;
 @property (weak) IBOutlet NSTextField *fruitTabTwo;
 @property (weak) IBOutlet NSTextField *fruitFieldOne;
 @property (weak) IBOutlet NSTextField *fruitFieldTwo;
+
+// declare the arrays which will hold strings, the fruits that user likes or dislikes
+
+@property (strong, nonatomic) NSMutableArray *fruitsUserLikes;
+@property (strong, nonatomic) NSMutableArray *fruitsUserDislikes;
+
+// declare the array that will hold all the possible fruits in the application
+
+@property (strong, nonatomic) NSArray *allFruits;
+
+// declare the actions - add to the first column, second, and also clear the first and the second column
 
 - (IBAction)addOne:(id)sender;
 - (IBAction)addTwo:(id)sender;
--- lab#1/lab#1/AppDelegate.m
+++ lab#1/lab#1/AppDelegate.m
@@ -1,35 +1,145 @@
 #import "AppDelegate.h"
 
 @implementation AppDelegate
+
+// synthesize every outlet and array
+
 @synthesize fruitTabOne = _fruitTabOne;
 @synthesize fruitTabTwo = _fruitTabTwo;
 @synthesize fruitFieldOne = _fruitFieldOne;
 @synthesize fruitFieldTwo = _fruitFieldTwo;
+@synthesize fruitsUserLikes = _fruitsUserLikes;
+@synthesize fruitsUserDislikes = _fruitsUserDislikes;
+@synthesize allFruits = _allFruits;
 
 @synthesize window = _window;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     NSLog(@"Application finished launching succesfully.");	
+    
+    // We have to set the fruit fields (text inputs)
+    // target to self, so that when user presses 
+    // enter the fruit will be added to the text label
+    
     [self.fruitFieldOne setTarget:self];
     [self.fruitFieldTwo setTarget:self];
     [self.fruitFieldOne setAction:@selector(addOne:)];
     [self.fruitFieldTwo setAction:@selector(addTwo:)];
+    
+    // initiate all permitted fruits
+    
+    _allFruits = [NSArray arrayWithObjects:@"Apple",@"Apricot",@"Avocado",@"Banana",@"Breadfruit",@"Blackberry",@"Blueberry",@"Cherry",@"Coconut",@"Eggplant",@"Grape",@"Grapefruit",@"Kiwi",@"Lemon",@"Lime",@"Mandarine",@"Mango",@"Melon",@"Orange",@"Peach",@"Pear",@"Pineapple",@"Tomato",@"Watermelon", nil];
+    
+    _fruitsUserLikes = [NSMutableArray array];
+    _fruitsUserDislikes = [NSMutableArray array];
 }
 
 - (IBAction)addOne:(id)sender {
-    NSLog(@"Should add one");
+    // add the fruit to the first column, if it exists
+    NSString *fruitName = [_fruitFieldOne stringValue];
+    BOOL fruitExists, fruitAlreadyInList;
+    for(NSString *fruit in _allFruits){
+        if ([fruit caseInsensitiveCompare:fruitName] == NSOrderedSame) { // if there exists such a fruit
+            
+            fruitExists = TRUE;
+            fruitAlreadyInList = FALSE;
+                // find out if the user already likes it
+            for(NSString *fruitUserLikes in _fruitsUserLikes){ 
+                if([fruitUserLikes caseInsensitiveCompare:fruitName] == NSOrderedSame){ // if user already likes it
+                    NSAlert *myAlert = [NSAlert alertWithMessageText:@"Error" defaultButton:@"OK" alternateButton:nil otherButton:nil informativeTextWithFormat:@"Sorry, that fruit is already in the list."];
+                    [myAlert beginSheetModalForWindow:_window modalDelegate:nil didEndSelector:nil contextInfo:nil];
+                    fruitAlreadyInList = TRUE;
+                    break;
+                    }
+                
+                }
+                // find out if the user already dislikes it
+                for(NSString *fruitUserDislikes in _fruitsUserDislikes){
+            if([fruitUserDislikes caseInsensitiveCompare:fruit] == NSOrderedSame){ // if user dislikes it
+                NSAlert *myAlert = [NSAlert alertWithMessageText:@"Error" defaultButton:@"OK" alternateButton:nil otherButton:nil informativeTextWithFormat:@"But you dislike that fruit!"];
+                [myAlert beginSheetModalForWindow:_window modalDelegate:nil didEndSelector:nil contextInfo:nil];
+                fruitAlreadyInList = TRUE;
+                break;
+                }
+            }
+                
+            if(!fruitAlreadyInList){ // if it's not in the list
+            NSString *newTab = [NSString stringWithFormat:[_fruitTabOne stringValue]];
+            newTab = [newTab stringByAppendingString:@"\n"];
+            newTab = [newTab stringByAppendingString:fruitName];
+            [_fruitTabOne setStringValue:newTab];
+            [_fruitsUserLikes addObject:fruitName]; // add to the likes array
+            }
+            break;
+        }
+        else {
+            fruitExists = FALSE;   
+        }
+    }
+        if(fruitExists == FALSE){
+            NSAlert *myAlert = [NSAlert alertWithMessageText:@"Error" defaultButton:@"OK" alternateButton:nil otherButton:nil informativeTextWithFormat:@"Sorry, that fruit doesn't exist."];
+            [myAlert beginSheetModalForWindow:_window modalDelegate:nil didEndSelector:nil contextInfo:nil];
+        }
 }
 
 - (IBAction)addTwo:(id)sender {
-    NSLog(@"Should add two");
+    // add the fruit to the second column, if it exists
+    NSString *fruitName = [_fruitFieldTwo stringValue];
+    BOOL fruitExists, fruitAlreadyInList;
+    for(NSString *fruit in _allFruits){
+        if ([fruit caseInsensitiveCompare:fruitName] == NSOrderedSame) { // if there exists such a fruit
+            
+            fruitExists = TRUE;
+            fruitAlreadyInList = FALSE;
+            // find out if the user already dislikes it
+            for(NSString *fruitUserDislikes in _fruitsUserDislikes){ 
+                if([fruitUserDislikes caseInsensitiveCompare:fruitName] == NSOrderedSame){ // if user already dislikes it
+                    NSAlert *myAlert = [NSAlert alertWithMessageText:@"Error" defaultButton:@"OK" alternateButton:nil otherButton:nil informativeTextWithFormat:@"Sorry, that fruit is already in the list."];
+                    [myAlert beginSheetModalForWindow:_window modalDelegate:nil didEndSelector:nil contextInfo:nil];
+                    fruitAlreadyInList = TRUE;
+                    break;
+                }
+                
+            }
+            // find out if the user already likes it
+            for(NSString *fruitUserLikes in _fruitsUserLikes){
+                if([fruitUserLikes caseInsensitiveCompare:fruit] == NSOrderedSame){ // if user dislikes it
+                    NSAlert *myAlert = [NSAlert alertWithMessageText:@"Error" defaultButton:@"OK" alternateButton:nil otherButton:nil informativeTextWithFormat:@"But you like that fruit!"];
+                    [myAlert beginSheetModalForWindow:_window modalDelegate:nil didEndSelector:nil contextInfo:nil];
+                    fruitAlreadyInList = TRUE;
+                    break;
+                }
+            }
+            
+            if(!fruitAlreadyInList){ // if it's not in the list
+                NSString *newTab = [NSString stringWithFormat:[_fruitTabTwo stringValue]];
+                newTab = [newTab stringByAppendingString:@"\n"];
+                newTab = [newTab stringByAppendingString:fruitName];
+                [_fruitTabTwo setStringValue:newTab];
+                [_fruitsUserDislikes addObject:fruitName]; // add to the likes array
+            }
+            break;
+        }
+        else {
+            fruitExists = FALSE;   
+        }
+    }
+    if(fruitExists == FALSE){
+        NSAlert *myAlert = [NSAlert alertWithMessageText:@"Error" defaultButton:@"OK" alternateButton:nil otherButton:nil informativeTextWithFormat:@"Sorry, that fruit doesn't exist."];
+        [myAlert beginSheetModalForWindow:_window modalDelegate:nil didEndSelector:nil contextInfo:nil];
+    }
 }
 
 - (IBAction)clearOne:(id)sender {
-    NSLog(@"Should clear one");
+    [_fruitTabOne setStringValue:@"Fruits you like:"]; // reset the text label
+    _fruitsUserLikes = nil; // reset the array
+    _fruitsUserLikes = [NSMutableArray array];
 }
 
 - (IBAction)clearTwo:(id)sender {
-    NSLog(@"Should clear two");
+    [_fruitTabTwo setStringValue:@"Fruits you dislike:"]; // reset the text label
+    _fruitsUserDislikes = nil; // reset the array
+    _fruitsUserDislikes = [NSMutableArray array];
 }
 @end
