3b233eb9acaede32cfcf0e2db85cbfd0f3d8ce83
Constantin Lungu <koctake@gmail.com>
2013-03-14 13:33:45 +0200

    Added alerts, sliders, a secure text field and custom menu buttons.

--- lab#2/lab#2/AppDelegate.h
+++ lab#2/lab#2/AppDelegate.h
@@ -1,7 +1,16 @@
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate> 
 
 @property (assign) IBOutlet NSWindow *window;
+@property (nonatomic) BOOL isHidden;
+@property (weak) IBOutlet NSSecureTextField *secureTextField;
+@property (weak) IBOutlet NSTextField *centralTextField;
 
+- (IBAction)alertPressed:(id)sender;
+- (IBAction)visitRepoPressed:(id)sender;
+- (IBAction)randomizePressed:(id)sender;
+- (IBAction)showHidePressed:(id)sender;
+
+- (void) passwordEntered;
 @end
--- lab#2/lab#2/AppDelegate.m
+++ lab#2/lab#2/AppDelegate.m
@@ -3,10 +3,50 @@
 @implementation AppDelegate
 
 @synthesize window = _window;
+@synthesize isHidden = _isHidden;
+@synthesize secureTextField = _secureTextField;
+@synthesize centralTextField = _centralTextField;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    _isHidden = FALSE;
+    [self.secureTextField setTarget:self];
+    [self.secureTextField setAction:@selector(passwordEntered)];
     
 }
 
+- (IBAction)alertPressed:(id)sender {
+    NSAlert *myAlert = [NSAlert alertWithMessageText:@"Alert" defaultButton:@"OK" alternateButton:nil otherButton:nil informativeTextWithFormat:@"Hey, you've pressed the alert button! Here you go with a notification."];
+    [myAlert beginSheetModalForWindow:_window modalDelegate:nil didEndSelector:nil contextInfo:nil];
+}
+
+- (IBAction)visitRepoPressed:(id)sender {
+    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"https://github.com/TUM-FAF/WP-FAF-111-Lungu-Constantin"]];
+}
+
+- (IBAction)randomizePressed:(id)sender {
+    [_window setAlphaValue:0.6 + ((double)(arc4random()%5)/5)];
+    CGRect myRect = CGRectMake(arc4random()%800, arc4random()%800, 400+arc4random()%400, 400+arc4random()%400);
+    [_window setFrame:NSRectFromCGRect(myRect) display:TRUE];
+}
+
+- (IBAction)showHidePressed:(id)sender {
+    if(!_isHidden){
+    [_window setIsVisible:FALSE];
+        _isHidden = TRUE;
+        }
+    else if(_isHidden){
+    [_window setIsVisible:TRUE];
+    _isHidden = FALSE;
+    }
+}
+
+- (void) passwordEntered{
+    if([self.secureTextField.stringValue caseInsensitiveCompare:@"constantin"] == NSOrderedSame){
+        NSRunAlertPanel(@"Correct!", @"You know my surname!", @"OK", nil, nil);
+    }
+    else {
+        NSRunAlertPanel(@"Wrong!", @"That's not my surname!", @"OK", nil, nil);
+    }
+}
 @end
